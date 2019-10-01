cf676dd5c46fc23eaa34731534e4910e4b77271f
shivendra143 <shivendrachaudhary05@gmail.com>
2019-10-01 16:29:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# integralui-accordion-overview-aka1gt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/integralui-accordion-overview-aka1gt)
---
+++ src/app/app.component.ts
@@ -0,0 +1,41 @@
+import { Component, ViewContainerRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { IntegralUIAccordion } from '@lidorsystems/integralui-web/bin/integralui/components/integralui.accordion';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ],
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent  {
+    public data: Array<any>;
+    public selGroup: any = null;
+
+    public ctrlStyle: any = {
+        general: {
+            normal: 'acc-ovw-normal'
+        }
+    }
+
+    constructor(){
+        this.data = [
+            { 
+                icon: 'icons library',
+                text: 'Books',
+                body: 'Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor.'
+            },
+            { 
+                icon: 'icons album',
+                text: 'Music',
+                body: 'Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat.'
+            },
+            { 
+                icon: 'icons star-empty',
+                text: 'Favorites',
+                body: 'Fusce convallis, mauris imperdiet gravida bibendum, nisl turpis suscipit mauris, sed placerat ipsum urna sed risus. In convallis tellus a mauris. Curabitur non elit ut libero tristique sodales. Mauris a lacus. Donec mattis semper leo. In hac habitasse platea dictumst. Fusce convallis, mauris imperdiet gravida bibendum, nisl turpis suscipit mauris, sed placerat ipsum urna sed risus. In convallis tellus a mauris. Curabitur non elit ut libero tristique sodales. Mauris a lacus. Donec mattis semper leo. In hac habitasse platea dictumst. Fusce convallis, mauris imperdiet gravida bibendum, nisl turpis suscipit mauris, sed placerat ipsum urna sed risus. In convallis tellus a mauris. Curabitur non elit ut libero tristique sodales. Mauris a lacus. Donec mattis semper leo. In hac habitasse platea dictumst.'
+            }
+        ];
+
+        this.selGroup = this.data[0];
+    }   
+}
